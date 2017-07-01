d361eb775ad652fbf72c309c752f0698282917e5
bestarti99 <praca.uek@outlook.com>
2017-07-01 11:59:25 +0200

    poprawki

--- js/main.js
+++ js/main.js
@@ -17,5 +17,27 @@ function scrolluj() {
     } else {
         $('#menu').removeClass('klasa');
     }
-
 }
+
+
+var root = $('body');
+$('a[href^="#"]').click(function(event) {
+    event.preventDefault();
+    var href = $(this).attr('href');
+    
+    root.animate({
+        scrollTop: $(href).offset().top - 50
+    }, 500);
+});
+
+/*
+
+function moveTo() {
+    
+    
+    // Scroll to a certain element
+    document.querySelector('#about-me').scrollIntoView({ 
+    behavior: 'smooth' 
+});
+}
+*/
